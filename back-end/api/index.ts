247008d7c20133bb6b4d26ef2fb5d8a4c1fa1771
Vu <vutran.0110204@gmail.com>
2026-03-17 16:45:09 +0700

    fix: connect db before handling vercel api

--- back-end/api/index.ts
+++ back-end/api/index.ts
@@ -1,3 +1,15 @@
 import app from '../src/app.js';
+import connectDB from '../src/config/db.js';
 
-export default app;
+let isConnected = false;
+
+const ensureDb = async () => {
+  if (isConnected) return;
+  await connectDB();
+  isConnected = true;
+};
+
+export default async function handler(req: any, res: any) {
+  await ensureDb();
+  return app(req, res);
+}
